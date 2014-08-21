7db7246bda2945a5b57c83f4f1992a0d58bc8417
M2000 <mchuprin@outlook.com>
2014-08-21 09:46:07 +0300

    new landing

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+function myFirstFunction(){
+    document.getElementById("I_could_text").innerHTML="I could possibly use my own eyes to look at the stars, thanks Starnight App...";
+    document.getElementById("photo").src="img/home_bl1_2.png";
+    document.getElementById("first_slider").style.backgroundColor="white";
+    document.getElementById("second_slider").style.backgroundColor="#5b6869";
+    document.getElementById("third_slider").style.backgroundColor="#5b6869";
+    }
+function mySecondFunction(){
+    document.getElementById("I_could_text").innerHTML="Hello, people!";
+    document.getElementById("photo").src="img/home_bl1_3.png";
+    document.getElementById("second_slider").style.backgroundColor="white";
+    document.getElementById("first_slider").style.backgroundColor="#5b6869";
+    document.getElementById("third_slider").style.backgroundColor="#5b6869";
+    }
+function myThirdFunction(){
+    document.getElementById("I_could_text").innerHTML="I will study JS fast!";
+    document.getElementById("photo").src="img/home_bl1_5.png";
+    document.getElementById("third_slider").style.backgroundColor="white";
+    document.getElementById("first_slider").style.backgroundColor="#5b6869";
+    document.getElementById("second_slider").style.backgroundColor="#5b6869";
+    }
+
